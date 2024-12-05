cde35c447b33f6741f7c96169a2a598eff445db7
Anandha Ragaven <ragavenr1301@gmail.com>
2024-12-05 18:12:53 -0500

    created main.py file, comleted pneumonia_detection_model.py by adding final summarize and train functions

--- pneumonia_detection_model.py
+++ pneumonia_detection_model.py
@@ -3,7 +3,7 @@ from keras.models import Sequential #Import Sequential model API
 from keras.layers import (Dense,Conv2D,MaxPooling2D, Dropout,BatchNormalization,Flatten,MaxPool2D) #IMporting required layers for CNN
 
 
-class PnemoniaDetection:
+class PneumoniaDetection:
 
     def __init__(self,input_shape=(256,256,3),learning_rate=0.001):
         """
@@ -56,5 +56,28 @@ class PnemoniaDetection:
         model.add(Dropout(0.2))
         model.add(MaxPool2D((2, 2), strides=2, padding = 'same'))
 
+        model.add(Flatten())  # flatten the model
 
-        
+        model.add(Dense(units=128, activation='relu'))  # Fully connected layer
+        model.add(Dropout(0.2))  # To prevent further overfitting
+
+        # Final output layer
+        model.add(Dense(units=1, activation='sigmoid'))  # single neuron with sigmoid function for binary classification
+
+        # compile the model
+        model.compile(optimizer=Adam(lr=self.learning_rate), loss='binary_crossentropy', metrics=['accuracy'])
+        #adam optimizer,binary crossentropy loss,  accuracy metrics
+
+        return model
+
+    def summarize(self):
+
+        #Print the summary of the model
+        self.model.summary()
+
+    def train(self, train_generator, validation_generator, epochs=10):
+        #Train the model with the given data generators.
+        history = self.model.fit(train_generator,
+                                    validation_data=validation_generator,
+                                    epochs=epochs)
+        return history
